10d821c227556b1752882b6ba04696d5f6374971
Sempatik35 <86168164+Sempatik35@users.noreply.github.com>
2022-05-27 19:51:37 +0300

    Update SpookScanner.py

--- SpookScanner.py
+++ SpookScanner.py
@@ -1,16 +1,34 @@
 import socket
 
-print("""  ___                _    ___                            
+import random,time, sys
+from colorama import *
+ 
+init(autoreset=True)
+fr = Fore.RED
+fb = Fore.BLUE
+fc = Fore.CYAN
+fw = Fore.WHITE
+fy = Fore.YELLOW
+fg = Fore.GREEN
+ 
+def logo():
+    clear = "\x1b[0m"
+    colors = [36, 32, 34, 35, 31, 37]
+ 
+    x = """
+  ___                _    ___                            
  / __|_ __  ___  ___| |__/ __| __ __ _ _ _  _ _  ___ _ _ 
  \__ \ '_ \/ _ \/ _ \ / /\__ \/ _/ _` | ' \| ' \/ -_) '_|
  |___/ .__/\___/\___/_\_\|___/\__\__,_|_||_|_||_\___|_|  
-     |_|                                                 
-     """)
-
-print("""                            
-             Created By SEMPATİK       
-         ╚────────────────────────────╝
-      """)
+     |_|                    
+ 
+       +--------CREATED BY SEMPATİK--------+
+ 
+                                          """
+    for N, line in enumerate(x.split("\n")):
+        sys.stdout.write(" \x1b[1;%dm%s%s\n " % (random.choice(colors), line, clear))
+        time.sleep(0.06)
+logo()
 
 site = input("Site url: ")
 target = socket.gethostbyname(site)
@@ -29,6 +47,7 @@ try:
         s.close()
 except Exception as e:
     print(Error , e)
+
 
 
 
